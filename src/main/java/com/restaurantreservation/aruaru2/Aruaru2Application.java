65dc0c166ea8869088931cbee81d2c1be9b7743d
KyeongHwanHwang <ghkdrudghks1@naver.com>
2022-08-29 14:58:24 +0900

    aruaru2

---
+++ src/main/java/com/restaurantreservation/aruaru2/Aruaru2Application.java
@@ -0,0 +1,13 @@
+package com.restaurantreservation.aruaru2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aruaru2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aruaru2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurantreservation/aruaru2/Aruaru2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantreservation.aruaru2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aruaru2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
